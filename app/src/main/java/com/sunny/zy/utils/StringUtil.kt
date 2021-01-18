9860694dead3ef7271d9df4c810fe0a8dac800eb
ZhangYe <zhangye98@foxmail.com>
2021-01-18 18:24:21 +0800

    金额格式化进行修改并扩展,支持传出格式化参数

--- app/src/main/java/com/sunny/zy/utils/StringUtil.kt
+++ app/src/main/java/com/sunny/zy/utils/StringUtil.kt
@@ -10,40 +10,79 @@ import java.util.*
 object StringUtil {
     /**
      * 金额格式化
-     * @param s 金额
+     * @param money 金额
      * @param len 小数位数
      * @return 格式后的金额
      */
 
-    fun insertComma(money: String?, len: Int): String {
+    fun formatMoney(money: String?, len: Int, pattern: String? = null): String {
         if (money == null || money.isEmpty() || money == "0.0" || money == "0" || money == "0.00") {
             return "0.00"
         }
         val num = money.toDouble()
-        val formater = if (len == 0) {
-            DecimalFormat("###,###")
 
+        if (pattern != null) {
+            val mFormat = DecimalFormat(pattern)
+            return mFormat.format(num)
+        }
+        val mFormat = if (len == 0) {
+            DecimalFormat("#")
         } else {
             val buff = StringBuffer()
-            buff.append("###,##0.")
+            buff.append("#.")
             for (i in 0 until len) {
                 buff.append("0")
             }
             DecimalFormat(buff.toString())
         }
-        return formater.format(num)
+        return mFormat.format(num)
     }
 
 
-    fun insertComma(money: Double): String = insertComma(money.toString(), 2)
-    fun insertComma(money: Float): String = insertComma(money.toString(), 2)
+    fun formatMoney(money: String?, pattern: String) = formatMoney(money, 0, pattern)
+    fun formatMoney(money: Double): String = formatMoney(money.toString(), 2)
+    fun formatMoney(money: Float): String = formatMoney(money.toString(), 2)
 
 
     //根据长度生成随机字符串
     fun getRandomChar(length: Int): String {            //生成随机字符串
         val chr = charArrayOf(
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            'A',
+            'B',
+            'C',
+            'D',
+            'E',
+            'F',
+            'G',
+            'H',
+            'I',
+            'J',
+            'K',
+            'L',
+            'M',
+            'N',
+            'O',
+            'P',
+            'Q',
+            'R',
+            'S',
+            'T',
+            'U',
+            'V',
+            'W',
+            'X',
+            'Y',
+            'Z'
         )
         val random = Random()
         val buffer = StringBuffer()

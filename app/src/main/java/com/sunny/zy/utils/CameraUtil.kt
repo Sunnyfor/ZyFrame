1933a4cb2b55fef28dd8380d5bd4d92c872823a8
ZhangYe <zhangye98@foxmail.com>
2021-02-22 17:47:50 +0800

    删除图片选择无用代码

--- app/src/main/java/com/sunny/zy/utils/CameraUtil.kt
+++ app/src/main/java/com/sunny/zy/utils/CameraUtil.kt
@@ -1,16 +1,12 @@
 package com.sunny.zy.utils
 
 import android.Manifest
-import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.sunny.zy.base.BaseActivity
 import com.sunny.zy.http.ZyConfig
@@ -25,21 +21,14 @@ class CameraUtil(private var activity: BaseActivity) {
 
     var onResultListener: OnResultListener? = null
 
-    private var messageMap = hashMapOf<String, String>()
-
     private var file: File? = null
     private var uri: Uri? = null
-    private var missPermission = ""
     private var aspectX = 500
     private var aspectY = 500
 
-
-    init {
-        messageMap[Manifest.permission.CAMERA] = "相机权限"
-        messageMap[Manifest.permission.READ_EXTERNAL_STORAGE] = "存储卡读取权限"
-        messageMap[Manifest.permission.WRITE_EXTERNAL_STORAGE] = "存储卡写入权限"
-    }
-
+    /**
+     * 跳转相机
+     */
     fun startCamera() {
         activity.requestPermissions(
             arrayOf(
@@ -48,11 +37,22 @@ class CameraUtil(private var activity: BaseActivity) {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
             )
         ) {
-            intentCamera()
+            initFile()
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            }
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)// 更改系统默认存储路径
+            activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                startPhotoZoom(uri ?: return@registerForActivityResult)
+            }.launch(intent)
         }
     }
 
 
+    /**
+     * 跳转相册
+     */
     fun startAlbum() {
         activity.requestPermissions(
             arrayOf(
@@ -60,40 +60,16 @@ class CameraUtil(private var activity: BaseActivity) {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
             )
         ) {
-            intentPhoto()
+            initFile()
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                //添加这一句表示对目标应用临时授权该Uri所代表的文件
+                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            }
+            activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                startPhotoZoom(it.data?.data ?: return@registerForActivityResult)
+            }.launch(intent)
         }
-    }
-
-
-    /**
-     * 跳转相机
-     */
-    private fun intentCamera() {
-        initFile()
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-        }
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)// 更改系统默认存储路径
-        activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            startPhotoZoom(uri ?: return@registerForActivityResult)
-        }.launch(intent)
-    }
-
-
-    /**
-     * 跳转相册
-     */
-    private fun intentPhoto() {
-        initFile()
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            //添加这一句表示对目标应用临时授权该Uri所代表的文件
-            intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-        }
-        activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            startPhotoZoom(it.data?.data ?: return@registerForActivityResult)
-        }.launch(intent)
     }
 
 
@@ -158,20 +134,5 @@ class CameraUtil(private var activity: BaseActivity) {
             }
         }
 
-    }
-
-    //检查所有权限是否拥有
-    private fun checkPermission(array: Array<String>): Boolean {
-        array.forEach {
-            if (ContextCompat.checkSelfPermission(
-                    activity,
-                    it
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                missPermission = it
-                return false
-            }
-        }
-        return true
     }
 }

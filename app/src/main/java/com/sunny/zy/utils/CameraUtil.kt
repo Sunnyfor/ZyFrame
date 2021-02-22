5e0957c445301ce9e39d2bc0fce41c81d51f4fec
ZhangYe <zhangye98@foxmail.com>
2021-02-22 17:15:32 +0800

    优化图片上传工具类

--- app/src/main/java/com/sunny/zy/utils/CameraUtil.kt
+++ app/src/main/java/com/sunny/zy/utils/CameraUtil.kt
@@ -1,12 +1,16 @@
 package com.sunny.zy.utils
 
+import android.Manifest
 import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.sunny.zy.base.BaseActivity
 import com.sunny.zy.http.ZyConfig
@@ -21,85 +25,81 @@ class CameraUtil(private var activity: BaseActivity) {
 
     var onResultListener: OnResultListener? = null
 
-    private val photoType = 100 //图片类型
-    private var isCrop = false //是否裁剪类型
-    private val cameraType = 102 //相机类型
+    private var messageMap = hashMapOf<String, String>()
+
     private var file: File? = null
     private var uri: Uri? = null
-    private var type = cameraType
-    private var aspectX = 1
-    private var aspectY = 1
-    private var outputX = 150
-    private var outputY = 150
+    private var missPermission = ""
+    private var aspectX = 500
+    private var aspectY = 500
 
-    //跳转相机
-    fun intentCamera() {
-        try {
-            type = cameraType
-            initFile()
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-            }
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)// 更改系统默认存储路径
-            activity.startActivityForResult(intent, cameraType)
-        } catch (e: ActivityNotFoundException) {
-            ToastUtil.show("没有检测到摄像头！")
+
+    init {
+        messageMap[Manifest.permission.CAMERA] = "相机权限"
+        messageMap[Manifest.permission.READ_EXTERNAL_STORAGE] = "存储卡读取权限"
+        messageMap[Manifest.permission.WRITE_EXTERNAL_STORAGE] = "存储卡写入权限"
+    }
+
+    fun startCamera() {
+        activity.requestPermissions(
+            arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        ) {
+            intentCamera()
         }
     }
 
 
-    //跳转相册
-    private fun intentPhoto(isCrop: Boolean) {
-        try {
-            this.isCrop = isCrop
-            this.type = photoType
-            initFile()
-            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                //添加这一句表示对目标应用临时授权该Uri所代表的文件
-                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-            }
-            activity.startActivityForResult(intent, type)
-        } catch (e: ActivityNotFoundException) {
-            ToastUtil.show("没有检测到相册！")
+    fun startAlbum() {
+        activity.requestPermissions(
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        ) {
+            intentPhoto()
         }
-
     }
 
-
-    fun intentPhoto() {
-        intentPhoto(false)
-    }
-
-
-    fun intentPhotoAndCrop() {
-        intentPhoto(true)
-    }
 
     /**
-     * 结果回调需要在Activity回调中调用
+     * 跳转相机
      */
-    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                cameraType -> {
-                    onResultListener?.onResult(file ?: return)
-
-                }
-                photoType -> {
-                    if (isCrop) {
-                        startPhotoZoom(data?.data ?: return)
-                    } else {
-                        onResultListener?.onResult(file ?: return)
-                    }
-                }
-            }
+    private fun intentCamera() {
+        initFile()
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
         }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)// 更改系统默认存储路径
+        activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            startPhotoZoom(uri ?: return@registerForActivityResult)
+        }.launch(intent)
     }
 
 
-    //裁剪图片
+    /**
+     * 跳转相册
+     */
+    private fun intentPhoto() {
+        initFile()
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //添加这一句表示对目标应用临时授权该Uri所代表的文件
+            intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        }
+        activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            startPhotoZoom(it.data?.data ?: return@registerForActivityResult)
+        }.launch(intent)
+    }
+
+
+    /**
+     * 裁剪图片
+     */
     private fun startPhotoZoom(mUri: Uri) {
         val intent = Intent("com.android.camera.action.CROP")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
@@ -113,25 +113,24 @@ class CameraUtil(private var activity: BaseActivity) {
 
         intent.putExtra("aspectX", aspectX)
         intent.putExtra("aspectY", aspectY)
-        intent.putExtra("outputX", outputX)
-        intent.putExtra("outputY", outputY)
+        if (aspectX != 0 && aspectY != 0) {
+            intent.putExtra("outputX", aspectX)
+            intent.putExtra("outputY", aspectY)
+        }
         intent.putExtra("return-data", false)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file))
         intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString())
         intent.putExtra("noFaceDetection", true) // no face detection
-        isCrop = false
-        activity.startActivityForResult(intent, cameraType)
+        activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            onResultListener?.onResult(file ?: return@registerForActivityResult)
+        }.launch(intent)
+
     }
 
 
     fun setAspectXY(aspectX: Int, aspectY: Int) {
         this.aspectX = aspectX
         this.aspectY = aspectY
-    }
-
-    fun setOutputXY(outputX: Int, outputY: Int) {
-        this.outputX = outputX
-        this.outputY = outputY
     }
 
 
@@ -144,8 +143,14 @@ class CameraUtil(private var activity: BaseActivity) {
     //初始化图片文件及URI
     private fun initFile() {
         //有权限
-        file = File(ZyConfig.TEMP, "IMG_${System.currentTimeMillis()}.jpg")
+        file = FileUtil.getFile("IMG_${System.currentTimeMillis()}.jpg")
         file?.let {
+            if (it.parentFile?.exists() == false) {
+                it.parentFile?.mkdirs()
+            }
+            if (!it.exists()) {
+                it.createNewFile()
+            }
             uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 FileProvider.getUriForFile(activity, ZyConfig.authorities, it)
             } else {
@@ -153,5 +158,20 @@ class CameraUtil(private var activity: BaseActivity) {
             }
         }
 
+    }
+
+    //检查所有权限是否拥有
+    private fun checkPermission(array: Array<String>): Boolean {
+        array.forEach {
+            if (ContextCompat.checkSelfPermission(
+                    activity,
+                    it
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                missPermission = it
+                return false
+            }
+        }
+        return true
     }
 }

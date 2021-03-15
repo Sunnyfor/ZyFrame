1a1834e8f605801d3b05abf9a50f33d619b07191
ZhangYe <zhangye98@foxmail.com>
2021-03-15 18:07:53 +0800

    修改Glide缓存路径为temp/glide

--- app/src/main/java/com/sunny/zy/utils/MyAppGlideModule.kt
+++ app/src/main/java/com/sunny/zy/utils/MyAppGlideModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.ModelLoader
 import com.bumptech.glide.load.model.ModelLoaderFactory
@@ -15,6 +16,7 @@ import com.sunny.zy.http.ZyConfig
 import com.sunny.zy.http.ZyHttp
 import okhttp3.Call
 import java.io.InputStream
+
 
 @GlideModule
 class MyAppGlideModule : AppGlideModule() {
@@ -32,6 +34,10 @@ class MyAppGlideModule : AppGlideModule() {
 
     override fun applyOptions(context: Context, builder: GlideBuilder) {
         builder.setLogLevel(ZyConfig.glideLogLevel)
+        val cacheSize = 100 * 1000L * 1000L
+        builder.setDiskCache(
+            DiskLruCacheFactory(FileUtil.getFile("glide").path, cacheSize)
+        )
     }
 
     class Factory(private val client: Call.Factory) : ModelLoaderFactory<GlideUrl, InputStream> {

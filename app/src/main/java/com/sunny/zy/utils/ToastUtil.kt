f5f9ba0f98646dd0d33e6778a808a9f5a0fefd5d
ZhangYe <zhangye98@foxmail.com>
2021-03-26 11:02:50 +0800

    修复toast工具类内存泄露问题

--- app/src/main/java/com/sunny/zy/utils/ToastUtil.kt
+++ app/src/main/java/com/sunny/zy/utils/ToastUtil.kt
@@ -1,5 +1,7 @@
 package com.sunny.zy.utils
 
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import com.sunny.zy.ZyFrameStore
 
@@ -12,16 +14,29 @@ object ToastUtil {
 
     private var toast: Toast? = null
 
+    private var LONG_DELAY = 3500
+    private var SHORT_DELAY = 2000
+
+    private val delay = SHORT_DELAY
+
+    private val handler = Handler(Looper.getMainLooper()) {
+        toast?.cancel()
+        toast = null
+        return@Handler false
+    }
+
     /**
      * 显示Toast
      * @param content Toast信息
      */
     fun show(content: String?, type: Int) {
+        handler.removeMessages(delay)
         toast?.cancel()
-        toast = null
         toast = Toast.makeText(ZyFrameStore.getContext(), content ?: "", type).apply {
             show()
         }
+        val delay = if (type == Toast.LENGTH_SHORT) SHORT_DELAY else LONG_DELAY
+        handler.sendEmptyMessageDelayed(delay, delay.toLong())
     }
 
     fun show(content: String?) {

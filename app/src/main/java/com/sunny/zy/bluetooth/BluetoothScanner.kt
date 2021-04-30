9f80013cb77e776c68fb7490495cf42c861d1da9
ZhangYe <zhangye98@foxmail.com>
2021-04-30 11:29:57 +0800

    扫描方法扩展

--- app/src/main/java/com/sunny/zy/bluetooth/BluetoothScanner.kt
+++ app/src/main/java/com/sunny/zy/bluetooth/BluetoothScanner.kt
@@ -6,15 +6,14 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanFilter
-import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
 import android.content.Intent
 import android.os.ParcelUuid
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
+import androidx.appcompat.app.AppCompatActivity
 import com.sunny.zy.ZyFrameStore
-import com.sunny.zy.base.BaseActivity
 import com.sunny.zy.utils.LogUtil
 import com.sunny.zy.utils.ToastUtil
 import java.util.*
@@ -29,7 +28,7 @@ import kotlin.collections.ArrayList
  */
 
 
-object BluetoothScanner{
+object BluetoothScanner {
     private var isScan = false
 
     var onResult: ScanCallback? = null
@@ -44,9 +43,10 @@ object BluetoothScanner{
      */
     @RequiresPermission(Manifest.permission.BLUETOOTH_ADMIN)
     fun startScan(
-        baseActivity: BaseActivity,
-        serviceId: String? = null,
-        onResult:ScanCallback
+        activity: AppCompatActivity,
+        filters: ArrayList<ScanFilter>? = null,
+        settings: ScanSettings? = null,
+        onResult: ScanCallback? = null
     ) {
         this.onResult = onResult
         if (bluetoothAdapter == null) {
@@ -55,19 +55,45 @@ object BluetoothScanner{
 
         if (!isScan) {
             if (bluetoothAdapter?.isEnabled == false) {
-                baseActivity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                     if (it.resultCode == Activity.RESULT_OK) {
-                        scan(serviceId)
+                        scan(filters, settings)
                     } else {
                         ToastUtil.show("请打开蓝牙后扫描！")
                     }
                 }.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
                 return
             }
-            scan(serviceId)
+            scan(filters, settings)
         } else {
             ToastUtil.show("蓝牙正在扫描中！")
         }
+    }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_ADMIN)
+    fun startScan(
+        activity: AppCompatActivity,
+        filters: ArrayList<ScanFilter>? = null,
+        onResult: ScanCallback? = null
+    ) {
+        startScan(activity, filters,null, onResult)
+    }
+
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_ADMIN)
+    fun startScan(
+        activity: AppCompatActivity,
+        serviceId: String? = null,
+        onResult: ScanCallback? = null
+    ) {
+        val bleScanFilters: ArrayList<ScanFilter> = ArrayList()
+        //添加过滤服务ID
+        serviceId?.let {
+            bleScanFilters.add(
+                ScanFilter.Builder().setServiceUuid(ParcelUuid(UUID.fromString(serviceId))).build()
+            )
+        }
+        startScan(activity, bleScanFilters, null, onResult)
     }
 
 
@@ -86,18 +112,10 @@ object BluetoothScanner{
      * 蓝牙扫描
      */
     @RequiresPermission(Manifest.permission.BLUETOOTH_ADMIN)
-    private fun scan(serviceId: String? = null) {
+    private fun scan(filters: ArrayList<ScanFilter>? = null, settings: ScanSettings? = null) {
         isScan = true
-
-        val bleScanFilters: ArrayList<ScanFilter> = ArrayList()
-        //添加过滤服务ID
-        serviceId?.let {
-            bleScanFilters.add(
-                ScanFilter.Builder().setServiceUuid(ParcelUuid(UUID.fromString(serviceId))).build()
-            )
-        }
         bluetoothAdapter?.bluetoothLeScanner?.startScan(
-            bleScanFilters, ScanSettings.Builder().build(), onResult
+            filters, settings ?: ScanSettings.Builder().build(), onResult
         )
         LogUtil.i("开始蓝牙扫描！")
     }
